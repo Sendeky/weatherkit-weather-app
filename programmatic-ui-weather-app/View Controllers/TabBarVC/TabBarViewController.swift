e5039173edb55b4a54557c25886b4da6e4994d93
Sendeky <spruslasha@gmail.com>
2023-01-09 16:17:30 -0800

    added swiping between viewcontrollers

--- programmatic-ui-weather-app/View Controllers/TabBarVC/TabBarViewController.swift
+++ programmatic-ui-weather-app/View Controllers/TabBarVC/TabBarViewController.swift
@@ -12,21 +12,29 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var swipeGetsure = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeGetsure.numberOfTouchesRequired = 1
+        swipeGetsure.direction = .right
+        view.addGestureRecognizer(swipeGetsure)
+        
+        swipeGetsure = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeGetsure.numberOfTouchesRequired = 1
+        swipeGetsure.direction = .left
+        view.addGestureRecognizer(swipeGetsure)
+        
+        
         //Create instance of view controllers
         let mainVC = MainViewController()
         let forecastVC = ForecastListVC()
         let infoVC = InfoVC()
-//        let settingsVC = SettingsListVC()
         
         //Set title
         mainVC.title = "Current"
         forecastVC.title = "Forecast"
         infoVC.title = "Info"
-//        settingsVC.title = "Settings"
         
         //Assign controllers to tab bar
         self.setViewControllers([mainVC, forecastVC, infoVC], animated: false)
-//        tabBar.backgroundColor = UIColor(red: 100.0/255.0, green: 50.0/255.0, blue: 235.0/255.0, alpha: 1.0)
         tabBar.isOpaque = true
         tabBar.backgroundColor = .clear
         let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialDark)
@@ -40,10 +48,19 @@ class TabBarViewController: UITabBarController {
         guard let items = self.tabBar.items else { return }
         let images = ["cloud.sun.rain.fill", "chart.bar.xaxis", "info.circle"]
         
-        for x in 0...2 {
-            items[x].image = UIImage(systemName: images[x])
+        for x in 0...2 { items[x].image = UIImage(systemName: images[x]) }
+    }
+    
+    @objc func swiped(_ gesture: UISwipeGestureRecognizer) {
+        if gesture.direction == .left {
+            if (self.selectedIndex) < 2 { // set your total tabs here
+                self.selectedIndex += 1
+            }
+        } else if gesture.direction == .right {
+            if (self.selectedIndex) > 0 {
+                self.selectedIndex -= 1
+            }
         }
-
     }
 }
 
