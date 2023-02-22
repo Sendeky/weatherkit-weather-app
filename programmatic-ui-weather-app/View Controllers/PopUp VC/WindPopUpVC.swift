72fb3974b16537b811b5854f0b4b8f24a8ad8ca7
Sendeky <83136978+Sendeky@users.noreply.github.com>
2023-02-22 13:50:42 -0800

    Started making Wind Popup VC

---
+++ programmatic-ui-weather-app/View Controllers/PopUp VC/WindPopUpVC.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import Charts
+
+
+struct WindPopUpVC: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+            //TODO:
+            //1. Wind Speed graph
+            //2. Background + Animations
+        } 
+    }
+}
+
+struct WindPopUpVC_Previews: PreviewProvider {
+    static var previews: some View {
+        WindPopUpVC()
+    }
+}
