fb6247b9a26b1888617ac443c9093526e9df8c60
Sendeky <spruslasha@gmail.com>
2023-11-01 12:53:19 -0700

    added a top stack to iPad view, changed how ViewControllers load in SceneDelegate.swift

---
+++ programmatic-ui-weather-app/iPad Version/iPadMainTopCurrentStack/iPadMainTopCurrentStack.swift
@@ -0,0 +1,92 @@
+//
+//  iPadMainTopCurrentStack.swift
+//  weatherkit-weather-app
+//
+//  Created by Ruslan Spirkin on 11/1/23.
+//
+
+import UIKit
+
+class iPadMainTopCurrentStack: UIView {
+
+    // MARK: START: stuff needed for UIView
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    // MARK: END
+    
+    
+    // Topmost stack
+    let topStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 15
+        return stack
+    }()
+    
+    // Current City Label
+    let currentCityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Location!"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // Top Info Stack
+    let topInfoStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 15
+        stack.distribution = .equalCentering
+        return stack
+    }()
+    
+    // Min Temperature Label
+    let minTempLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Min Temp"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // Current Temp Label
+    let currentTempLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Current Temp"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // Max Temperature Label
+    let maxTempLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Max Temp"
+        return label
+    }()
+
+
+    private func setupUI() {
+        topInfoStack.addArrangedSubview(minTempLabel)
+        topInfoStack.addArrangedSubview(currentTempLabel)
+        topInfoStack.addArrangedSubview(maxTempLabel)
+        
+        topStack.addArrangedSubview(currentCityLabel)
+        topStack.addArrangedSubview(topInfoStack)
+        
+        addSubview(topStack)
+        
+        // Really important
+        minTempLabel.translatesAutoresizingMaskIntoConstraints = false
+        currentTempLabel.translatesAutoresizingMaskIntoConstraints = false
+        maxTempLabel.translatesAutoresizingMaskIntoConstraints = false
+        currentCityLabel.translatesAutoresizingMaskIntoConstraints = false
+        topInfoStack.translatesAutoresizingMaskIntoConstraints = false
+        topStack.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
--- programmatic-ui-weather-app/iPad Version/iPadMainViewController.swift
+++ programmatic-ui-weather-app/iPad Version/iPadMainViewController.swift
@@ -8,6 +8,56 @@
 import Foundation
 import UIKit
 
-class iPadMainViewController: MainViewController {
+class iPadMainViewController: UIViewController {
     // Contents will be components
+    
+    // MARK: Top Current Stack Component
+    let customView = iPadMainTopCurrentStack()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // sets SVG background
+        setBackground()
+        // sets up the UI
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .orange
+        view.addSubview(customView)
+        
+        
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.topAnchor),
+            customView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            customView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            customView.topStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            customView.topStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            customView.topStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 250),
+        ])
+    }
+    
+    private func setBackground() {
+        let background = UIImage(named: "Background.svg")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        
+        // add subview here so that layout anchors have view to constrain to
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: view.topAnchor, multiplier: 1.05),
+            imageView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: 1.05),
+            imageView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: -1.0),
+            imageView.trailingAnchor.constraint(equalToSystemSpacingAfter: view.trailingAnchor, multiplier: 1.05)
+        ])
+        view.sendSubviewToBack(imageView)
+    }
+
 }
